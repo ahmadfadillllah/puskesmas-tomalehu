f11c38814c312e6e882d22a6ad40b2f02c0af334
ahmadfadillllah <ahmadfadillah502@gmail.com>
2022-05-14 11:58:45 +0800

    Update RuangWA

--- app/Http/Controllers/PasienLamaController.php
+++ app/Http/Controllers/PasienLamaController.php
@@ -77,25 +77,25 @@ class PasienLamaController extends Controller
         $update =  DB::table('pin_activation')->where('id', $dataPasien->id) ->update(['kode' => Str::random(5)]);
 
         $kode  = pinActivation::all()->where('id', $dataPasien->id)->first();
-        // $token = "NZxnya6kAvgta8FSReRykr5ZoD9kGrYxy3t4m5T99zfj2xBmYp";
-        // $phone = $dataPasien->no_hp; //atau bisa menggunakan 62812xxxxxxx
-        // $message = "Hai $dataPasien->nama_lengkap, Kode Aktivasi adalah [$kode->kode], Mohon untuk tidak menyebarkan kode tersebut";
-        // $messageid= "2EFD576575BF1741C3530xxxxxxxxx"; //optional
+        $token = "NZxnya6kAvgta8FSReRykr5ZoD9kGrYxy3t4m5T99zfj2xBmYp";
+        $phone = $dataPasien->no_hp; //atau bisa menggunakan 62812xxxxxxx
+        $message = "Hai $dataPasien->nama_lengkap, Kode Aktivasi adalah [$kode->kode], Mohon untuk tidak menyebarkan kode tersebut";
+        $messageid= "2EFD576575BF1741C3530xxxxxxxxx"; //optional
 
-        // $curl = curl_init();
-        // curl_setopt_array($curl, array(
-        // CURLOPT_URL => 'https://app.ruangwa.id/api/send_express',
-        // CURLOPT_RETURNTRANSFER => true,
-        // CURLOPT_ENCODING => '',
-        // CURLOPT_MAXREDIRS => 10,
-        // CURLOPT_TIMEOUT => 0,
-        // CURLOPT_FOLLOWLOCATION => true,
-        // CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        // CURLOPT_CUSTOMREQUEST => 'POST',
-        // CURLOPT_POSTFIELDS => 'token='.$token.'&number='.$phone.'&message='.$message.'&messageid='.$messageid,
-        // ));
-        // $response = curl_exec($curl);
-        // curl_close($curl);
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://app.ruangwa.id/api/send_express',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => 'token='.$token.'&number='.$phone.'&message='.$message.'&messageid='.$messageid,
+        ));
+        $responsewa = curl_exec($curl);
+        curl_close($curl);
 
         $accesskey= "8b77591fe1ae830044d4cd1f96923d84";
         $phone = $dataPasien->no_hp; //atau bisa menggunakan 62812xxxxxxx
@@ -113,10 +113,10 @@ class PasienLamaController extends Controller
         CURLOPT_CUSTOMREQUEST => 'POST',
         CURLOPT_POSTFIELDS => 'accesskey='.$accesskey.'&number='.$phone.'&message='.$message,
         ));
-        $response = curl_exec($curl);
+        $responsesms = curl_exec($curl);
         curl_close($curl);
 
-       if($response){
+       if($responsewa or $responsesms){
 
            return redirect()->route('pasienLama')->with('notification', 'Berhasil Mengupdate Kode');
        }else{
